5834c54972166d26a200935840ff547f2e085b67
AiDpiv <aidpiv@gmail.com>
2017-05-28 17:02:26 +0300

    adding only res

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   root "projects#index"
-  resources :projects
+  resources :projects, only: [:show, :index]
   # get 'projects/index'
   # get 'project/:id', to: 'projects#show'
 end
