71fea4b85fac5eccc3eb94084baddad84f646e3b
AiDpiv <aidpiv@gmail.com>
2017-06-02 22:51:42 +0300

    deployment

--- config/deploy.rb
+++ config/deploy.rb
@@ -23,7 +23,7 @@ set :deploy_to, '/home/deploy/archintouch'
 
 # Default value for :linked_files is []
 # append :linked_files, "config/database.yml", "config/secrets.yml"
-append :linked_files, "config/database.yml", "config/secrets.yml"
+append :linked_files, "config/database.yml", "config/secrets.yml", "db/production.sqlite3"
 
 # Default value for linked_dirs is []
 # append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
