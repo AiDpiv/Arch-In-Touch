99125e9dc126a363c7c5a69ef309d4761ee9b1c7
AiDpiv <aidpiv@gmail.com>
2017-06-04 11:30:57 +0300

    disable x-scroll

--- config/deploy.rb
+++ config/deploy.rb
@@ -27,7 +27,7 @@ append :linked_files, "config/database.yml", "config/secrets.yml", "db/productio
 
 # Default value for linked_dirs is []
 # append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
-append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "vendor/bundle", "public/system", "public/uploads", "public/production/"
+append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "vendor/bundle", "public/system", "public/uploads", "public/production"
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
